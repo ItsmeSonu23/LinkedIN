351cbb91956780710d46325ea9c3bcc64e32f43c
Sonu Mishra <155150337+ItsmeSonu23@users.noreply.github.com>
2025-06-16 21:38:30 +0530

    Seturn ClickedIn setup for frontend and backed

---
+++ backend/src/main/java/com/clickedin/clickedin/ClickedinApplication.java
@@ -0,0 +1,13 @@
+package com.clickedin.clickedin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickedinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickedinApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/clickedin/clickedin/ClickedinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clickedin.clickedin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickedinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
