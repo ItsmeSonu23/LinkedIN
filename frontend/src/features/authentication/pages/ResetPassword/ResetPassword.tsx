5acb53ba243511ea8b36ecfcc87d9d711e7ef86d
Sonu Mishra <155150337+ItsmeSonu23@users.noreply.github.com>
2025-06-21 12:38:17 +0530

    ResetPassword and Signup pages: Update error handling and UI feedback

--- frontend/src/features/authentication/pages/ResetPassword/ResetPassword.tsx
+++ frontend/src/features/authentication/pages/ResetPassword/ResetPassword.tsx
@@ -8,15 +8,66 @@ export const ResetPassword = () => {
   const navigate = useNavigate()
   const [emailSent, setEmailSent] = useState(false)
   const [errorMessage, setErrorMessage] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+  const[email, setEmail] = useState<string>("");
+
+  const sendPasswordResetToken = async (email: string) => {
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/v1/authentication/send-password-reset-token?email=${email}`, {
+        method: "PUT",
+      });
+      if (response.ok) {
+        setEmailSent(true);
+        setErrorMessage("");
+        return;
+      }
+      const { message } = await response.json();
+      setErrorMessage(message);
+
+    } catch (error) {
+      console.error(error);
+      setErrorMessage("An unexpected error occurred. Please try again.");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  const resetPassword = async (email: string, code: string, newPassword: string) => {
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/v1/authentication/reset-password?email=${email}&token=${code}&newPassword=${newPassword}`, {
+        method: "PUT",
+      });
+      if (response.ok) {
+        setErrorMessage("");
+        navigate("/login");
+      }
+      const { message } = await response.json();
+      setErrorMessage(message);
+    } catch (error) {
+      console.error(error);
+      setErrorMessage("An unexpected error occurred. Please try again.");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <Box>
       <h1 className="text-[2rem] font-bold mb-4">Reset Password</h1>
       {
-        !emailSent ? <form>
+        !emailSent ? <form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            setIsLoading(true);
+            const email = (e.currentTarget.email as HTMLInputElement).value;
+            await sendPasswordResetToken(email);
+            setEmail(email);
+            setIsLoading(false);
+          }}>
           <p>Enter your email and we'll send a verification code if it matches an existing CheckedIn account.</p>
           <Input name="email" type="email" label="Email" />
           <p className="text-red-500 text-sm mb-4">{errorMessage}</p>
-          <Button type="submit" className="mt-4" onClick={()=>setEmailSent(true)}>
+          <Button type="submit" className="mt-4" onClick={() => setEmailSent(true)}>
             Send Verification Code
           </Button>
           <Button type="button" outline
@@ -24,9 +75,19 @@ export const ResetPassword = () => {
             onClick={() => navigate("/login")}>
             Back
           </Button>
-        </form> : <form>
+        </form> : <form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            setIsLoading(true);
+            const code = (e.currentTarget.code as HTMLInputElement).value;
+
+            const newPassword = (e.currentTarget.newPassword as HTMLInputElement).value;
+            await resetPassword(email, code, newPassword);
+            setIsLoading(false);
+          }
+        >
           <p>Enter the verification code we sent to your email and your new password.</p>
-          <Input  type="text" label="Verification Code" />
+          <Input type="text" label="Verification Code" />
           <Input type="password" label="New Password" />
 
           <p className="text-red-500 text-sm mb-4">{errorMessage}</p>
@@ -35,7 +96,10 @@ export const ResetPassword = () => {
           </Button>
           <Button type="button" outline
             className="mt-4"
-            onClick={() => setEmailSent(false)}>
+            onClick={() =>{
+              setErrorMessage("");
+              setEmailSent(false);
+            }}>
             Back
           </Button>
         </form>
--- frontend/src/features/authentication/pages/Signup/Signup.tsx
+++ frontend/src/features/authentication/pages/Signup/Signup.tsx
@@ -1,17 +1,39 @@
-import { NavLink } from "react-router"
+import { NavLink, useLocation, useNavigate } from "react-router"
 import { Box } from "../../components/Box/Box"
 import { Button } from "../../components/Button/Button"
 import { Input } from "../../components/Input/Input"
 import { Separator } from "../../components/Separator/Separator"
 import { useState } from "react"
+import { useAuthentication } from "../../contexts/AuthenticationContextProvider"
 
 export const Signup = () => {
-  const[errorMessage, setErrorMessage] = useState<string | null>("jhxcb");
+  const[errorMessage, setErrorMessage] = useState<string | null>("");
+  const[isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+  const {signup} = useAuthentication();
+  const doSignup = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+    const email = (e.currentTarget.email as HTMLInputElement).value;
+    const password = (e.currentTarget.password as HTMLInputElement).value;
+    try{
+      await signup(email, password);
+      navigate("/");
+    }catch (error) {
+      if(error instanceof Error) {
+        setErrorMessage(error.message);
+      }else{
+        setErrorMessage("An unexpected error occurred.");
+      }
+    }finally {
+      setIsLoading(false);
+    }
+  }
   return (
     <Box>
         <h1 className="text-[2rem] font-bold mb-4">Sign up</h1>
         <p>Make the most of your professional life.</p>
-        <form>
+        <form onSubmit={doSignup} className="flex flex-col gap-2 mt-8">
             <Input type="email" id="email" label="Email" />
             <Input type="password" id="password" label="Password" />
 
@@ -23,7 +45,7 @@ export const Signup = () => {
             }
 
             <p className="text-xs">By clicking Agree & Join or Continue, you agree to CheckedIn's <a className="text-violet-600 font-bold" href="">User Agreement</a>, <a className="text-violet-600 font-bold" href="">Privacy Policy</a>, and <a className="text-violet-600 font-bold" href="">Cookie Policy</a>.</p>
-            <Button type="submit" >Agree & Join</Button>
+            <Button disabled={isLoading} type="submit" >Agree & Join</Button>
         </form>
         <Separator>Or</Separator>
         <div className="text-center w-full">
